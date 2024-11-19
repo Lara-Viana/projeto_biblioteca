97d892f986c268252f244f39b3144b8ca187dbd0
Lara-Viana <laradomingos257@gmail.com>
2024-11-19 14:17:30 -0300

    final

--- app/views.py
+++ app/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from .models import *
 from django.views import View
+from django.contrib import messages
+#from django.views import View
+
 class IndexView(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'index.html')
+    def post(self, request):
+        pass
+
+#class IndexView(View):
+#def get(self, request, *args, **kwargs):
+#return render(request, 'index.html')
+
 class LivrosView(View):
     def get(self, request, *args, **kwargs):
         livros = Livro.objects.all()
@@ -12,23 +22,30 @@ class LivrosView(View):
 class EmprestimoView(View):
     def get(self, request, *args, **kwargs):
         reservas = Emprestimo.objects.all()
-        return render(request, 'reserva.html',{'reservas': reservas})
+        return render(request, 'reserva.html',
+{'reservas': reservas})
+
 class CidadesView(View):
     def get(self, request, *args, **kwargs):
         cidades = Cidade.objects.all()
         return render(request, 'cidade.html', {'cidades': cidades})
+    
 class AutoresView(View):
-    def get(self, request, *args, **kwargs): 
+    def get(self, request, *args, **kwargs):
         autores = Autor.objects.all()
         return render(request, 'autor.html', {'autores': autores})
+    
 class EditorasView(View):
     def get(self, request, *args, **kwargs):
         editoras = Editora.objects.all()
         return render(request, 'editora.html', {'editoras': editoras})
+    
 class LeitoresView(View):
     def get(self, request, *args, **kwargs):
         leitores = Leitor.objects.all()
-        return render(request, 'leitor.html',{'leitores': leitores})
+        return render(request, 'leitor.html',
+{'leitores': leitores})
+    
 class GenerosView(View):
     def get(self, request, *args, **kwargs):
         generos = Genero.objects.all()
